50fdb78b7653193a31dd2bcee2dd2f13da5c0312
Veera <sveera.2001@gmail.com>
2022-08-06 20:28:58 -0400

    Read file to string

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use std::fs;
+fn read_file(name: &str) -> String {
+    fs::read_to_string(name)
+        .expect("Unable to read file.")
+}
+    
+fn main() {
+    let test_file = "/home/veera/Projects/orgp/test/example.org";
+    let contents = read_file(test_file);
+    println!("{contents}");
+}
