57923ecc7cf6b7899dfdccbd3655c0f3c9882c46
Veera <sveera.2001@gmail.com>
2023-03-16 14:17:12 -0400

    Parse unordered list

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,13 @@ mod parser;
 use std::fs;
 
 fn read_file(name: &str) -> String {
-    fs::read_to_string(name)
-        .expect("Unable to read file.")
+    fs::read_to_string(name).expect("Unable to read file.")
 }
-    
+
 fn main() {
     let test_file = "/home/veera/Projects/orgp/test/example.org";
     let contents = read_file(test_file);
-    let mut parser = parser::Parser::new();
-    let org = parser.parse(&contents);
+    let mut parser = parser::Parser::new(&contents);
+    let org = parser.parse();
     println!("{org:?}");
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,43 +1,65 @@
-#[derive(Default)]
-pub struct Parser {
-    elements: Vec<OrgEle>,
+use std::iter::Peekable;
+use std::str::Lines;
+
+pub struct Parser<'a> {
+    // elements: Vec<OrgEle>,
+    lines: Peekable<Lines<'a>>,
 }
 
 #[derive(Debug, Clone)]
 pub enum OrgEle {
     Title(String),
     Date(String),
+    UnOrderedList(Vec<String>),
 }
 
-impl Parser {
-    pub fn new() -> Self {
+impl<'a> Parser<'a> {
+    pub fn new(contents: &'a str) -> Self {
         Self {
-            elements: vec![],
+            lines: contents.lines().peekable(),
         }
     }
 
-    fn parse_title(&mut self, line: &str) {
-        let title = line.strip_prefix("#+TITLE:").unwrap();
-        self.elements.push(OrgEle::Title(title.trim().to_string()));
+    fn parse_title(&mut self) -> OrgEle {
+        let title =
+            self.lines.next().unwrap().strip_prefix("#+TITLE:").unwrap();
+        OrgEle::Title(title.trim().to_string())
     }
 
-    
-    fn parse_date(&mut self, line: &str) {
-        let date = line.strip_prefix("#+DATE:").unwrap();
-        self.elements.push(OrgEle::Date(date.trim().to_string()));
+    fn parse_date(&mut self) -> OrgEle {
+        let date =
+            self.lines.next().unwrap().strip_prefix("#+DATE:").unwrap();
+        OrgEle::Date(date.trim().to_string())
     }
 
-    pub fn parse(&mut self, contents: &str) -> Vec<OrgEle> { 
-        for line in contents.lines() {
-            if line.starts_with("#+TITLE:") {
-                self.parse_title(line);
-            } else if line.starts_with("#+DATE:") {
-                self.parse_date(line);
+    fn parse_unordered_list(&mut self) -> OrgEle {
+        let mut list = vec![];
+        while let Some(line) = self.lines.peek() {
+            if line.starts_with("- ") {
+                let sentence = line.strip_prefix("- ").unwrap();
+                list.push(sentence.trim().to_string());
+                self.lines.next();
             } else {
-                continue
+                break;
             }
         }
-        self.elements.clone()
+        OrgEle::UnOrderedList(list.clone())
+    }
+
+    pub fn parse(&mut self) -> Vec<OrgEle> {
+        let mut elements = vec![];
+        while let Some(line) = self.lines.peek() {
+            if line.starts_with("#+TITLE:") {
+                elements.push(self.parse_title());
+            } else if line.starts_with("#+DATE:") {
+                elements.push(self.parse_date());
+            } else if line.starts_with("- ") {
+                elements.push(self.parse_unordered_list());
+            } else {
+                self.lines.next();
+                continue;
+            }
+        }
+        elements.clone()
     }
 }
-
